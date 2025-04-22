73edf53cc547aea3a2ad48f977db80bb03278b6c
Michał Dąbrowski <michal.dabrowski@elympics.ai>
2025-04-22 23:07:43 +0200

    chore: bump dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/nerzh/swift-extensions-pack.git", .upToNextMajor(from: "2.0.0")),
+      .package(url: "https://github.com/mdab121/swift-extensions-pack.git", .upToNextMajor(from: "2.1.0")),
     ],
     
     targets: [
